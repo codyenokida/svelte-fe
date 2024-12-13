6b0d68e4b0c476b5f0a6d7c8b14e5bdf6e2341ff
Kota Cody Enokida <codyenokida@gmail.com>
2024-12-13 09:06:12 -0800

    some error handling

--- src/routes/simulation/history/+server.ts
+++ src/routes/simulation/history/+server.ts
@@ -3,8 +3,13 @@ import { json } from '@sveltejs/kit';
 const FAST_API_BASE_URL = import.meta.env.VITE_FAST_API_BASE_URL;
 
 export async function GET() {
-    const response = await fetch(`${FAST_API_BASE_URL}/simulations`);
-    const result = await response.json();
+    try {
+        const response = await fetch(`${FAST_API_BASE_URL}/simulations`);
+        const result = await response.json();
 
-    return json(result);
+        return json(result);
+    } catch (error) {
+        console.error(error);
+        return json({ error: 'Failed to fetch simulations' }, { status: 500 });
+    }
 }
